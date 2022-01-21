6c3887ec345d6bb0d367c9ff3c5fa70838842d87
mohanad-alkrunz <muhannd@ahdtech.com>
2022-01-21 23:02:15 +0200

    Generate Lot id

--- update_lot_id/models/mrp_production.py
+++ update_lot_id/models/mrp_production.py
@@ -6,17 +6,18 @@ from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
 from datetime import datetime
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
-
+from dateutil import tz
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     def action_generate_serial(self):
         self.ensure_one()
+
         self.lot_producing_id = self.env['stock.production.lot'].create({
             'product_id': self.product_id.id,
             'company_id': self.company_id.id,
-            'name': "Test",
+            'name': self.product_id.gen_lot_code(),
         })
         if self.move_finished_ids.filtered(lambda m: m.product_id == self.product_id).move_line_ids:
             self.move_finished_ids.filtered(
