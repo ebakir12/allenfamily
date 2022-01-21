fc203e11dfad8d1a08f8dbd3b86438bf3250d53e
mohanad-alkrunz <muhannd@ahdtech.com>
2022-01-21 22:43:41 +0200

    Edit Generate lot id

---
+++ update_lot_id/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Manufacturing Create Lot ID',
+    'version': '1.0.2.0.0',
+    'license': 'AGPL-3',
+    'author': 'AHED TECHNOLOGY',
+    'website': 'https://www.pptssolutions.com',
+    'category': 'Manufacturing',
+    'depends': ['mrp', 'product', 'stock', 'resource', 'web'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ update_lot_id/models/mrp_production.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import math
+from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
+from odoo.exceptions import UserError
+from datetime import datetime
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    def action_generate_serial(self):
+        self.ensure_one()
+        self.lot_producing_id = self.env['stock.production.lot'].create({
+            'product_id': self.product_id.id,
+            'company_id': self.company_id.id,
+            'name': "Test",
+        })
+        if self.move_finished_ids.filtered(lambda m: m.product_id == self.product_id).move_line_ids:
+            self.move_finished_ids.filtered(
+                lambda m: m.product_id == self.product_id).move_line_ids.lot_id = self.lot_producing_id
+        if self.product_id.tracking == 'serial':
+            self._set_qty_producing()
+
+
